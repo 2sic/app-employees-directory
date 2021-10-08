c7c401448856f7a20a08c387072f00d56a253a53
Roman Opalko <roman.opalko@2sic.com>
2021-10-08 13:05:20 +0200

    renamed google maps lib, folder structre updated

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,4 +1,4 @@
-import { activeGoogleMaps } from "./google-maps/google-maps";
+import { activeGoogleMaps } from "./lib-2sxc-google-maps";
 
 // so it can be called from the HTML when content re-initializes dynamically
 const winAny = (window as any);
